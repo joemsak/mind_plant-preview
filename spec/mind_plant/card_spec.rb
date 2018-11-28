83bcb615b960be3b43fa52769fc27cbb02501dcb
Joe Sak <joe@joesak.com>
2018-11-28 15:53:30 -0600

    Rearrange the spec

--- spec/mind_plant/card_spec.rb
+++ spec/mind_plant/card_spec.rb
@@ -1,20 +1,22 @@
 require "spec_helper"
 
 RSpec.describe MindPlant::Card do
-  describe ".new" do
-    let(:valid_number) { '4111111111111111' }
-    let(:number_validator) { double(:NumberValidator) }
+  let(:valid_number) { '4111111111111111' }
+  let(:number_validator) { double(:NumberValidator) }
 
-    subject do
-      MindPlant::Card.new('Joseph Sak', valid_number, 10_000, number_validator)
-    end
+  subject do
+    MindPlant::Card.new('Joseph Sak', valid_number, 10_000, number_validator)
+  end
 
+  describe "attributes" do
     its(:name)             { is_expected.to eq('Joseph Sak') }
     its(:number)           { is_expected.to eq(valid_number) }
     its(:limit)            { is_expected.to eq(10_000) }
     its(:number_validator) { is_expected.to eq(number_validator) }
     its(:balance)          { is_expected.to eq(0) }
+  end
 
+  describe "#valid?" do
     it "validates its numbers with the validator" do
       expect(number_validator).to receive(:valid?).with(valid_number).and_return(true)
       expect(subject.valid?).to be true
