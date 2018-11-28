bd5c0818275bca7f0429d16763ed214e2bcafa1b
Joe Sak <joe@joesak.com>
2018-11-28 15:44:42 -0600

    Validate with DI'd validator, default is Luhn

--- lib/mind_plant/card.rb
+++ lib/mind_plant/card.rb
@@ -2,10 +2,17 @@ module MindPlant
   class Card
     attr_accessor :name, :number, :limit
 
-    def initialize(name, number, limit)
+    attr_reader :number_validator
+
+    def initialize(name, number, limit, number_validator = Luhn)
       @name = name
       @number = number
       @limit = limit
+      @number_validator = number_validator
+    end
+
+    def valid?
+      number_validator.valid?(number)
     end
   end
 end
--- spec/mind_plant/card_spec.rb
+++ spec/mind_plant/card_spec.rb
@@ -2,12 +2,21 @@ require "spec_helper"
 
 RSpec.describe MindPlant::Card do
   describe ".new" do
+    let(:valid_number) { '4111111111111111' }
+    let(:number_validator) { double(:NumberValidator) }
+
     subject do
-      MindPlant::Card.new('Joseph Sak', '4111111111111111', 10_000)
+      MindPlant::Card.new('Joseph Sak', valid_number, 10_000, number_validator)
     end
 
     its(:name) { is_expected.to eq('Joseph Sak') }
-    its(:number) { is_expected.to eq('4111111111111111') }
+    its(:number) { is_expected.to eq(valid_number) }
     its(:limit) { is_expected.to eq(10_000) }
+    its(:number_validator) { is_expected.to eq(number_validator) }
+
+    it "validates its numbers with the validator" do
+      expect(number_validator).to receive(:valid?).with(valid_number).and_return(true)
+      expect(subject.valid?).to be true
+    end
   end
 end
