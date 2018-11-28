d63259dc0d2fb2fc321ecba40b75a0d70714d066
Joe Sak <joe@joesak.com>
2018-11-28 16:02:20 -0600

    Add charging to the balance

--- lib/mind_plant/card.rb
+++ lib/mind_plant/card.rb
@@ -2,13 +2,14 @@ module MindPlant
   class Card
     attr_accessor :name, :number, :limit
 
-    attr_reader :number_validator
+    attr_reader :number_validator, :charges
 
     def initialize(name, number, limit, number_validator = Luhn)
       @name = name
       @number = number
       @limit = limit
       @number_validator = number_validator
+      @charges = []
     end
 
     def valid?
@@ -16,7 +17,16 @@ module MindPlant
     end
 
     def balance
-      0
+      charges.sum { |c| c[:amount] }
+    end
+
+    def charge(amount)
+      if balance + amount <= limit
+        @charges.push({
+          processed_at: Time.now.to_i,
+          amount: amount
+        })
+      end
     end
   end
 end
--- spec/mind_plant/card_spec.rb
+++ spec/mind_plant/card_spec.rb
@@ -14,12 +14,40 @@ RSpec.describe MindPlant::Card do
     its(:limit)            { is_expected.to eq(10_000) }
     its(:number_validator) { is_expected.to eq(number_validator) }
     its(:balance)          { is_expected.to eq(0) }
+    its(:charges)          { are_expected.to be_empty }
   end
 
   describe "#valid?" do
     it "validates its numbers with the validator" do
       expect(number_validator).to receive(:valid?).with(valid_number).and_return(true)
       expect(subject.valid?).to be true
+    end
+  end
+
+  describe "#charge" do
+    it "adds to the list of charges" do
+      Timecop.freeze do
+        subject.charge(10)
+        expect(subject.charges).to eq([
+          { processed_at: Time.now.to_i, amount: 10 }
+        ])
+      end
+    end
+
+    it "affects the balance" do
+      subject.charge(10)
+      subject.charge(9.99)
+      subject.charge(18.76)
+
+      expect(subject.balance).to eq(38.75)
+    end
+
+    it "ignores charges which would increase the balance the limit" do
+      expect {
+        subject.charge(10_000.01)
+      }.not_to change {
+        subject.balance
+      }
     end
   end
 end
